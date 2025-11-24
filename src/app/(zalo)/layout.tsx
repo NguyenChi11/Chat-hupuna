c37f35c37b9c6d63435fa38b52950d4a836dd77a
NguyenChi11 <chinguyen280200@gmail.com>
2025-11-24 13:29:48 +0700

    [fix]: create and edit folder

---
+++ src/app/(zalo)/layout.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import LayoutBase from '../../components/layout/layout';
+
+export default function ZaloLayout({ children }: { children: React.ReactNode }) {
+  return <LayoutBase>{children}</LayoutBase>;
+}
